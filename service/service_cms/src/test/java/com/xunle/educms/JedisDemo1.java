1ceddf8ea9dbf05f8bcc9b6073da0bb533e9e8ae
Xunle <1601315809@qq.com>
2021-07-18 20:54:04 +0800

    首次修改

--- service/service_cms/src/test/java/com/xunle/educms/JedisDemo1.java
+++ service/service_cms/src/test/java/com/xunle/educms/JedisDemo1.java
@@ -3,11 +3,13 @@ package com.xunle.educms;
 import redis.clients.jedis.Jedis;
 
 /**
+ * 测试redis连接
  * @author xunle
  */
+
 public class JedisDemo1 {
     public static void main(String[] args) {
-        Jedis jedis = new Jedis("192.168.248.132", 6379);
+        Jedis jedis = new Jedis("127.0.0.1", 6379);
 
         String ping = jedis.ping();
         System.out.println(ping);
--- service/service_msm/src/test/java/com/xunle/edumsm/EmailTest.java
+++ service/service_msm/src/test/java/com/xunle/edumsm/EmailTest.java
@@ -5,22 +5,28 @@ import org.apache.commons.mail.HtmlEmail;
 import org.junit.Test;
 
 /**
+ * 邮箱测试
  * @author xunle
  */
 public class EmailTest {
 
     @Test
     public void send() {
-
+        //收件人
+        String target = " ";
+        //发件人
+        String source = " ";
+        //邮箱授权码
+        String auth = " ";
         try {
             HtmlEmail email = new HtmlEmail();
             email.setHostName("smtp.qq.com");
             email.setCharset("utf-8");
 
-            email.addTo("1614990979@qq.com");
-            email.setFrom("1601315809@qq.com", "xunle");
+            email.addTo(source);
+            email.setFrom(source, "xunle");
 
-            email.setAuthentication("1601315809@qq.com","xpnsajskvfokhdic");
+            email.setAuthentication(source,auth);
 
             email.setSubject("测试");
             email.setMsg("测试测试测试测试测试测试测试测试测试测试");
